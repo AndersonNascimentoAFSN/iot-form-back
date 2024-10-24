565fc00328d6be2b418eb5ebd76a5bdf6785b9ea
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-10-23 23:03:30 -0300

    feat: seed prod

---
+++ src/db/seed-education-levels.ts
@@ -0,0 +1,24 @@
+import { client, db } from './index'
+import { participants, educationLevels } from './schema'
+
+  ; (async function seed() {
+    await db.delete(participants)
+    await db.delete(educationLevels)
+
+    await db.insert(educationLevels).values([
+      { levelName: 'Ensino Fundamental (Completo)' },
+      { levelName: 'Ensino Fundamental (Incompleto)' },
+      { levelName: 'Ensino Médio (Completo)' },
+      { levelName: 'Ensino Médio (Incompleto)' },
+      { levelName: 'Curso Técnico (Completo)' },
+      { levelName: 'Curso Técnico (em andamento)' },
+      { levelName: 'Superior (completo)' },
+      { levelName: 'Superior (em andamento)' },
+      { levelName: 'Pós-Graduação (especialização)' },
+      { levelName: 'Mestrado' },
+      { levelName: 'Doutorado' },
+      { levelName: 'Outro' },
+    ])
+      .returning()
+      .execute()
+  })().finally(() => client.end())
